9390aa10d59fceb9426496cf235b4725a6cb1bcb
Jack Bjelland <jdbjelland@gmail.com>
2023-03-09 14:39:43 -0500

    Communication between FSR and server working

--- client/client.py
+++ client/client.py
@@ -3,6 +3,8 @@
 import socket
 import struct
 import rfid
+import RPi.GPIO as GPIO
+import threading as th
 
 HOST = "169.254.185.103"  # The server's hostname or IP address
 PORT = 5005  # The port used by the server
@@ -10,17 +12,64 @@ PORT = 5005  # The port used by the server
 IoO = 1
 CUID = 12345678
 
-count = 0
+#Interrupt routine
+push = 18
+fsr1 = 31
+fsr2 = 37
+press = 0
+run = 1
 
-#packet = struct.pack('?i', IoO, CUID)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(push, GPIO.IN)
+GPIO.setup(fsr1, GPIO.IN)
+GPIO.setup(fsr2, GPIO.IN)
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        #while True:
-        input("Press enter to continue:") #Replaces interrupt
+lock1 = 1
+lock2 = 1
+
+def FSR1():
+        global lock1
+        global press
+        lock1 = 0
+        print("FSR1 Tripped (Counting up)")
+        doSend(1)
+        while GPIO.input(fsr1):
+            temp = 1
+        press = press + 1
+        print("Count: ", press)
+        lock1 = 1
+
+def FSR2():
+        global lock2
+        global press
+        lock2 = 0
+        print("FSR2 Tripped (Counting down)")
+        doSend(0)
+        while GPIO.input(fsr2):
+            temp = 1
+        press = press - 1
+        print("Count: ", press)
+        lock2 = 1
+
+def STOP():
+        run = 0
+
+def doSend(IoO):
         CUID = rfid.GetCUID()
         packet = struct.pack('?8s', IoO, CUID)
         s.sendall(packet)
+
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #print("Awaiting connection...")
+        s.connect((HOST, PORT))
+        #print("Connected")
+        while True:
+            if GPIO.input(fsr1) and lock1:
+                    F1 = th.Thread(target=FSR1)
+                    F1.start()
+            if GPIO.input(fsr2) and lock2:
+                    F2 = th.Thread(target=FSR2)
+                    F2.start()
 
 
--- server/website/connection/server.py
+++ server/website/connection/server.py
@@ -21,7 +21,7 @@ def remove_car(ID,DB_Conn):
         else:
             print("sql call to remove car")
             #sql call to remove correspodign ID from the database
-            delete_RFID(DB_Conn,ID)
+            #delete_RFID(DB_Conn,ID)
 
 def add_car(ID,DB_Conn):
         if(ID == '11111111'):
@@ -29,7 +29,7 @@ def add_car(ID,DB_Conn):
         else:
             print("sql call to add car")
             #sql call to add corresponding ID from Database
-            insert_RFID(DB_Conn,ID)
+            #insert_RFID(DB_Conn,ID)
 
 #db_conn = sqlite3.connect("db.sq")
 IoO = 2
@@ -46,14 +46,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected by {addr}")
         while True:
-            data = conn.recv(12)
+            data = conn.recv(9)
             if not data:
-                break
+                continue
             else:
-                print(struct.unpack('?ii',data))
+                print(struct.unpack('?8s',data))
                 IoO = data[0]
                 CUID = data[1]
-                count = data[2]
                 updateDatabase(IoO, CUID)
 
 #conn.sendall(data)
