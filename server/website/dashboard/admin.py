476eaabedb9965537149269d7a0b27b2f96c5d89
garceauemily <86163864+garceauemily@users.noreply.github.com>
2023-03-09 13:42:22 -0500

    initial commit

---
+++ server/website/dashboard/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from dashboard.models import RFID
+from dashboard.models import LotSize
+# Register your models here.
+
+# Admin site
+# Databases
+class RFIDAdmin(admin.ModelAdmin):
+    list_display = ('Lot','RFID')
+    list_filter = ('Lot',)
+admin.site.register(RFID, RFIDAdmin)
+
+class LotSizesAdmin(admin.ModelAdmin):
+    list_display=('name','num_spaces','percentage_full')
+admin.site.register(LotSize,LotSizesAdmin)
+# Register your models here.
---
+++ server/website/testConnection.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print("connection made")
+    except Error as e:
+        print(e)
+
+    return conn
+
+def select_all_tasks(conn):
+    """
+    Query all rows in the auth_user table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM chartjs_rfid")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+def insert_RFID(conn,RFID_str):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO chartjs_rfid (Lot,RFID) VALUES ('C02',?)",(RFID_str,))
+    conn.commit()
+
+def delete_RFID(conn,RFID_str):
+    cur = conn.cursor()
+    cur.execute("DELETE FROM chartjs_rfid WHERE RFID=?",(RFID_str,))
+    conn.commit()
+
+#conn = create_connection("db.sqlite3")
+# select_all_tasks(conn)
+# insert_RFID(conn)
+# select_all_tasks(conn)
+# #delete_RFID(conn)
+# select_all_tasks(conn)
