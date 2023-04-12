cedf1a13e45796406ba61ac78837a734a2184b96
Jack Bjelland <jdbjelland@gmail.com>
2023-04-11 20:41:25 -0400

    New client model

--- client/client.py
+++ client/client.py
@@ -6,7 +6,7 @@ import rfid
 import RPi.GPIO as GPIO
 import threading as th
 
-HOST = "169.254.185.103"  # The server's hostname or IP address
+HOST = "192.168.187.123"  # The server's hostname or IP address
 PORT = 5005  # The port used by the server
 
 IoO = 1
@@ -16,6 +16,7 @@ CUID = 12345678
 push = 18
 fsr1 = 31
 fsr2 = 37
+led  = 16
 press = 0
 run = 1
 
@@ -23,6 +24,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(push, GPIO.IN)
 GPIO.setup(fsr1, GPIO.IN)
 GPIO.setup(fsr2, GPIO.IN)
+GPIO.setup(led, GPIO.OUT)
 
 lock1 = 1
 lock2 = 1
@@ -57,19 +59,30 @@ def STOP():
 def doSend(IoO):
         CUID = rfid.GetCUID()
         packet = struct.pack('?8s', IoO, CUID)
-        s.sendall(packet)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((HOST, PORT))
+                s.sendall(packet)
+                s.settimeout(5)
+                data = s.recv(4)
+                if data == b"full":
+                        toggleLED(1)
+                else:
+                        toggleLED(0)
+                s.close()
+
+def toggleLED(full):
+      if full == 1:
+        GPIO.output(led, GPIO.HIGH)
+      else:
+        GPIO.output(led, GPIO.LOW)
 
 while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        #print("Awaiting connection...")
-        s.connect((HOST, PORT))
-        #print("Connected")
-        while True:
-            if GPIO.input(fsr1) and lock1:
-                    F1 = th.Thread(target=FSR1)
-                    F1.start()
-            if GPIO.input(fsr2) and lock2:
-                    F2 = th.Thread(target=FSR2)
-                    F2.start()
-
-
+        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #         s.connect((HOST, PORT))
+        #         while True:
+        if GPIO.input(fsr1) and lock1:
+                F1 = th.Thread(target=FSR1)
+                F1.start()
+        if GPIO.input(fsr2) and lock2:
+                F2 = th.Thread(target=FSR2)
+                F2.start()
