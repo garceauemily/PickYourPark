034d37fba376e294d0fa300f43d51c5d450a6154
garceauemily <86163864+garceauemily@users.noreply.github.com>
2023-04-14 14:20:56 -0400

    added corresponding RFIDs/CUIDs/usernames

--- server/website/dashboard/admin.py
+++ server/website/dashboard/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from dashboard.models import Space
 from dashboard.models import LotSize
+from dashboard.models import RFID
 # Register your models here.
 
 # Admin site
@@ -14,4 +15,7 @@ admin.site.register(Space, SpaceAdmin)
 class LotSizesAdmin(admin.ModelAdmin):
     list_display=('name','num_spaces','percentage_full')
 admin.site.register(LotSize,LotSizesAdmin)
-# Register your models here.
+
+class RFIDAdmin(admin.ModelAdmin):
+    list_display = ('RFID','CUID','Username')
+admin.site.register(RFID,RFIDAdmin)
